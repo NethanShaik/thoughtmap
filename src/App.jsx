c4e19fe36fe7c737693195403096e945346d8a13
NethanShaik <nethanshaik@gmail.com>
2026-01-26 15:29:38 -0500

    Created frontend with text area and placeholder for results

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState("");
+  const [result, setResult] = useState(null);
+
+  const analyzeText = () => {
+    setResult({
+      main: "This text discusses AI explainability.",
+      intent: "The author wants to educate readers",
+      controversy: "Raises concerns about AI transparency."
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div style={{padding:"40px"}}>
+        <h1>ThoughtMap AI</h1>
+        <textarea
+          rows="6"
+          style={{width:"100%"}}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          />
+        <br /><br />
+        <button onClick={analyzeText}>
+          Analyze
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {result &&(
+        <div>
+          <h3>Main Idea</h3>
+          <p>{result.main}</p>
+
+          <h3>Author Intent</h3>
+          <p>{result.intent}</p>
+
+          <h3>Potential Controversy</h3>
+          <p>{result.controversy}</p>
+        </div>
+      )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
