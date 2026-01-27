43fc856de9aec4840a618608fdeb65ce094afb6f
NethanShaik <nethanshaik@gmail.com>
2026-01-26 23:47:25 -0500

    Created frontend UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -14,31 +13,47 @@ function App() {
   }
 
   return (
-      <div style={{padding:"40px"}}>
-        <h1>ThoughtMap AI</h1>
+    <div className="min-h-screen w-[100vw] bg-gradient-to-b from-blue-900 via-blue-950 to-black text-white">
+
+
+
+      <div className="w-full max-w-5xl mx-auto px-6 py-10 flex flex-col items-center gap-6 text-center">
+        <h1 className='text-4xl font-bold text-green-400'> ThoughtMap AI </h1>
+        <p className='text-grey-300'>
+          Paste text and we'll visualize how the model understands it.
+        </p>
         <textarea
+        className="w-full p-4 rounded-lg bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-green-400"
           rows="6"
-          style={{width:"100%"}}
+          placeholder='Paste your text here...'
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
           />
         <br /><br />
-        <button onClick={analyzeText}>
+        <button onClick={analyzeText}
+        className="px-6 py-2 bg-green-500 text-black font-semibold rounded-lg hover:bg-green-400 transition">
           Analyze
         </button>
       {result &&(
-        <div>
-          <h3>Main Idea</h3>
-          <p>{result.main}</p>
+        <div className="grid gap-4 pt-4">
+          <div className="p-4 bg-gray-800 rounded-lg border border-gray-700">
+            <h3 className="font-semibold text-green-300 mb-1">Main Idea</h3>
+            <p className="text-gray-300">{result.main}</p>
+          </div>
 
-          <h3>Author Intent</h3>
-          <p>{result.intent}</p>
+          <div className="p-4 bg-gray-800 rounded-lg border border-gray-700">
+            <h3 className="font-semibold text-green-300 mb-1">Author Intent</h3>
+            <p className='text-grey-300'>{result.intent}</p>
+          </div>
 
-          <h3>Potential Controversy</h3>
-          <p>{result.controversy}</p>
+          <div className="p-4 bg-gray-800 rounded-lg border border-gray-700">
+            <h3 className="font-semibold text-green-300 mb-1">Potential Controversy</h3>
+            <p className='text-grey-300'>{result.controversy}</p>
         </div>
+      </div>
       )}
       </div>
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // tell Tailwind to scan your React files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
